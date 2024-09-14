5191b6c7ac2769754e71d40eabb587c1f7e88064
Nav Ane Eth <allurinavaneeth30@gmail.com>
2024-09-14 12:11:06 +0530

    all set for backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,42 +2,38 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        pic: {
-            type: String,
-            default: 'https://w7.pngwing.com/pngs/831/88/png-transparent-user-profile-computer-icons-user-interface-mystique-miscellaneous-user-interface-design-smile-thumbnail.png',
-        },
-
+  {
+    name: { type: "String", required: true },
+    email: { type: "String", unique: true, required: true },
+    password: { type: "String", required: true },
+    pic: {
+      type: "String",
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
-    {
-        timestamp: true,
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
     },
+  },
+  { timestaps: true }
 );
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-userSchema.pre("save", async function (next){ // this function is used to save the password in encrypted type in our database
-    if(!this.isModified) {
-        next();
-    } 
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
